c60f87f078669989db0aab2a030786ec7d60f72b
Vladimir <ecofovka@gmail.com>
2020-11-16 18:40:10 +0100

    project is done

--- week2/project/hackyourweather/src/App.js
+++ week2/project/hackyourweather/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import Cities from './cities';
+import { useState } from "react";
+import "./App.css";
+import Cities from "./Cities";
+import Search from "./Search";
 
 function App() {
+  const [weatherData, setWeatherData] = useState([]);
   return (
-    <div id='app'>
-      <Cities/>
+    <div id="app">
+      <h1>Weather</h1>
+      <Search weatherData={weatherData} setWeatherData={setWeatherData} />
+      {weatherData.length > 0 ? <Cities weatherData={weatherData} /> : ""}
     </div>
   );
 }
--- week2/project/hackyourweather/src/cities.js
+++ week2/project/hackyourweather/src/cities.js
@@ -1,29 +1,33 @@
-import citiesData from './city-weather.json';
-import './Cities.css';
+// import citiesData from "./city-weather.json";
+import "./App.css";
 
+const CityCard = ({ city }) => {
+  const { name, sys, weather, main, coord } = city;
+  return (
+    <div className="cityCard">
+      <h1>
+        {name}, {sys.country}
+      </h1>
+      <h4>{weather[0].main}</h4>
+      <p>{weather[0].description}</p>
+      <div className="temp">
+        <p>min temp: {Math.round((main.temp_min - 273.15) * 10) / 10}</p>
+        <p>max temp: {Math.round((main.temp_max - 273.15) * 10) / 10}</p>
+      </div>
+      <p>
+        Location: {coord.lon}, {coord.lat}
+      </p>
+    </div>
+  );
+};
 
-const CityCard = ({city}) => {
-    const { name, sys, weather, main, coord } = city;
-    return (
-        <div className='cityCard'>
-            <h1>{name}, {sys.country}</h1>
-            <h4>{weather[0].main}</h4>
-            <p>{weather[0].description}</p>
-            <div className='temp'>
-                <p>min temp: {Math.round((main.temp_min - 273.15)*10)/10}</p>
-                <p>max temp: {Math.round((main.temp_max - 273.15)*10)/10}</p>
-            </div>
-            <p>Location: {coord.lon}, {coord.lat}</p>
-        </div>
-    );
-}
-
-const Cities = () => {
-    return (
-        <div id='citiesWrapper'>
-            <h1>Weather</h1>
-            {citiesData.map(city => <CityCard city={city}/>)}
-        </div>
-    );
-}
+const Cities = ({ weatherData }) => {
+  return (
+    <div id="citiesWrapper">
+      {weatherData.map((city) => {
+        return <CityCard city={city} key={city.id} />;
+      })}
+    </div>
+  );
+};
 export default Cities;
